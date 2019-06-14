ee9bc536dd214613bb6373787c481bffd267171c
Rodrigo Ferreira <rodrigoferreirasilva1997@hotmail.com>
2019-06-13 22:38:02 -0300

    Inserindo dados Automaticamente no Banco

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -1,13 +1,35 @@
 package com.nelioalves.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.repositores.RepositoryCategorias;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private RepositoryCategorias categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		Categoria cat3 = new Categoria(null, "Manutencao");
+		Categoria cat4 = new Categoria(null, "Sistemas");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4));
+		
+	}
+	
 
 }
