77e9505a5cb9c70f9d8fa9ebbce3d161dce1d34f
Rodrigo Ferreira <rodrigoferreirasilva1997@hotmail.com>
2019-07-19 20:42:29 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/nelioalves/cursomc/Services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/Services/CategoriaService.java
@@ -20,4 +20,9 @@ public class CategoriaService {
 		return categ.orElseThrow(() -> new ObjectNotFundExcepion(
 				"Objeto não encontrado! Id: " + id + ". Tipo: " + Categoria.class.getName()));
 	}
+	
+	public Categoria inserir(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
 }
--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.nelioalves.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nelioalves.cursomc.Services.CategoriaService;
 import com.nelioalves.cursomc.domain.Categoria;
@@ -22,6 +26,17 @@ public class CategoriaResource {
 		Categoria objBusca = service.buscarCategoria(id);
 		return ResponseEntity.ok().body(objBusca);
 		
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> inserir(@RequestBody Categoria obj){
+		obj = service.inserir(obj);
+		URI uri = ServletUriComponentsBuilder
+											.fromCurrentRequest()
+											.path("/{id}")	                //metodo para recuperar uri e concatenar com uri inicial
+											.buildAndExpand(obj.getId())
+											.toUri(); 
+		return ResponseEntity.created(uri).build();
 	}
 }
   
